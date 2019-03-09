802781560e7a2f552c5564727febd0c0d3063809
Maverobot <quzhengrobot@gmail.com>
2019-03-09 21:37:00 +0100

    added go stuff installation

--- setup.sh
+++ setup.sh
@@ -4,10 +4,19 @@ sudo apt install xclip build-essential cmake python3-dev silversearcher-ag
 
 sudo snap install --channel=edge shellcheck
 
+# Go stuffs
+sudo snap install go --classic
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v github.com/alecthomas/gometalinter
+gometalinter --install --update
+
 git submodule update --init --recursive
 
 if [ -d "ycmd" ]; then
     cd ycmd || return
-    git submodule update --init --recursive
     python3 build.py --clang-completer
 fi
