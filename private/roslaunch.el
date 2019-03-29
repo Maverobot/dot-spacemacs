9cb011ffd5305bcd0dd38e5f88e11aec9f941808
Zheng Qu <zheng.qu@franka.de>
2019-03-30 00:02:58 +0100

    added jump to dir

--- private/roslaunch.el
+++ private/roslaunch.el
@@ -1,5 +1,9 @@
 ;; Open launch/yaml file in new buffer
-(defun jump-in-launch-file ()
+
+(setq re-ros-path "\\$(find [^ ]*)[^ ]*\\.\\(launch\\|yaml\\|xacro\\)")
+(setq re-pkg "pkg=\"\\([^\"]*\\)\"")
+
+(defun jump-to-file ()
   (interactive)
   (setq current-line (thing-at-point 'line t))
   (setq found-match (string-match re-ros-path current-line))
@@ -11,16 +15,18 @@
         (setq no-package (string-match "\\[rospack\\] Error: package .* not found" absolute-path))
         (if no-package
             (message (match-string 0 absolute-path))
-          (find-file absolute-path)
-          )
-        )
-    )
-  )
+          (find-file absolute-path)))))
 
-(setq re-ros-path "\\$(find [^ ]*)[^ ]*\\.\\(launch\\|yaml\\|xacro\\)")
-(setq re-pkg "pkg=\"\\([^\"]*\\)\"")
-(setq re-name "name=\"\\([^\"]*\\)\"")
-(setq re-type "type=\"\\([^\"]*\\)\"")
+(defun jump-to-pkg-dir ()
+  (interactive)
+  (setq current-line (thing-at-point 'line t))
+  (setq found-match (string-match re-pkg current-line))
+  (if found-match
+      (progn
+        (setq pkg-name (match-string 1 current-line))
+        (setq absolute-path
+              (replace-regexp-in-string "\n$" "" (shell-command-to-string (format "rospack find %s" pkg-name))))
+        (find-file (concat absolute-path "/")))))
 
 (defun replace-in-string (pattern replacement original-text)
   (replace-regexp-in-string (regexp-quote pattern) replacement original-text nil 'literal))
@@ -28,7 +34,8 @@
 (when (fboundp 'nxml-mode)
   (defun my-launch-file-config ()
     "For use in `nxml-mode-hook'."
-    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gg" 'jump-in-launch-file)
-    )
-  (add-hook 'nxml-mode-hook 'my-launch-file-config)
+    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gg" 'jump-to-file)
+    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gp" 'jump-to-pkg-dir)
   )
+  (add-hook 'nxml-mode-hook 'my-launch-file-config)
+)
