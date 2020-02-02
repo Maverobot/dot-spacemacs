3e41dd5f13bd39b58b397fef53ed65dcfc715c59
Maverobot <quzhengrobot@gmail.com>
2020-02-02 01:43:46 +0100

    CHANGE: modulize script

--- build_ccls.sh
+++ build_ccls.sh
@@ -6,9 +6,10 @@ if [ -d "ccls" ]; then
 	cd ccls || return
 	if [ -d llvm ]; then rm -Rf llvm; fi
 	mkdir -p llvm && cd llvm || return
-	wget https://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-	tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+	LLVM_BASENAME="clang+llvm-9.0.0-$(uname -m)-linux-gnu-ubuntu-$(lsb_release -rs)"
+	wget "https://releases.llvm.org/9.0.0/${LLVM_BASENAME}.tar.xz"
+	tar -xf "${LLVM_BASENAME}.tar.xz"
 	cd ..
-	cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=./llvm/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04
+	cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="./llvm/${LLVM_BASENAME}"
 	cmake --build Release -- "-j$(nproc)"
 fi
