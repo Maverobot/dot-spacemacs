7660ec5fd981d9113ff6247b989301643a5425f7
Zheng Qu <quzhengrobot@gmail.com>
2021-06-09 10:51:48 +0200

    fix: compile groovy language server before using it

--- installer.sh
+++ installer.sh
@@ -100,6 +100,6 @@ echo "Note: if ~/.emacs.d or ~/.spacemacs.d already exists, it will be moved to 
 install_dependencies
 install_spacemacs
 compile_ccls
+compile_groovy_language_server
 update_spacemacs
 install_fonts
-compile_groovy_language_server
