438a1fd826a50761ec6832e6b5115ea1d957c519
Zheng Qu <quzhengrobot@gmail.com>
2021-11-04 23:35:12 +0100

    chore: Add back golang dependencies into the install script

--- installer.sh
+++ installer.sh
@@ -62,6 +62,22 @@ function install_dependencies {
     nvm install v10.19.0
     nvm use v10.19.0
 
+    # Install golang dependencies
+    sudo snap install go --classic
+    GO111MODULE=on go install golang.org/x/tools/gopls@latest
+    GO111MODULE=on CGO_ENABLED=0 go install -v -trimpath -ldflags '-s -w' github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    go install golang.org/x/tools/cmd/godoc@latest
+    go install golang.org/x/tools/cmd/goimports@latest
+    go install golang.org/x/tools/cmd/gorename@latest
+    go install golang.org/x/tools/cmd/guru@latest
+    go install github.com/cweill/gotests/...@latest
+    go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
+    go install github.com/fatih/gomodifytags@latest
+    go install github.com/godoctor/godoctor@latest
+    go install github.com/haya14busa/gopkgs/cmd/gopkgs@latest
+    go install github.com/josharian/impl@latest
+    go install github.com/rogpeppe/godef@latest
+
     # Install marked for generating html from markdown
     npm install -g marked
 
